4d51deeee839b08a5f969eb2ccbb4274ee4a904b
Tuan Phan Le <coderbillzay@gmail.com>
2023-11-25 20:42:31 +0700

    add gateway config

--- src/main/java/com/capstone/CapstoneApplication.java
+++ src/main/java/com/capstone/CapstoneApplication.java
@@ -2,6 +2,9 @@ package com.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,4 +14,5 @@ public class CapstoneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
+
 }
